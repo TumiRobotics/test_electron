cf94dff4985838c641c33ca9766ddc7ed146b135
Jaime Del Alcazar <jdelalcazar@utec.edu.pe>
2022-12-21 19:47:55 -0500

    template electron

---
+++ src/About.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function About() {
+  return (
+    <div className="page">
+      <h1>This is ABOUT page</h1>
+      <Link to='/'>Go to HOME</Link>
+    </div>
+  );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { createBrowserHistory, createHashHistory } from 'history';
+import { isElectron } from './utils';
+import Home from './Home';
+import About from './About';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+export const history = isElectron()
+  ? createHashHistory()
+  : createBrowserHistory();
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router history={history}>
+      <Routes>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/about' exact element={<About />}> </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
